5b415ebd0455189b0cf7590ccf944e6b30184851
Akshay Sachdeva <asachdeva@alumni.utexas.net>
2025-04-21 15:54:31 -0500

    more updates

--- flake.nix
+++ flake.nix
@@ -1,26 +1,55 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat={ url = "github:edolstra/flake-compat"; flake = false; };
   };
 
-  outputs = { self, nixpkgs, pre-commit-hooks, flake-utils, flake-compat }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let 
-     pkgs = import nixpkgs { inherit system; };
-      in with pkgs; {
-        devShells.default = mkShell {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.mkFlake {
+      inherit self nixpkgs;
+
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]; # Add more if needed
+
+      devShells.default = nixpkgs.lib.mkShell {
+        name = "development";
+        packages = with nixpkgs; [
+          haskell.ghc
+          rustc
+          cargo
+          fourmolu
+          rustfmt
+          clickhouse-client
+          helm
+          hyperdx-cli
+          argocd
+        ];
+        # You can add shellHook for environment setup if needed
+        shellHook = ''
+          echo "Welcome to the development shell for shila-jeet "
+        '';
+      };
+
+      packages = rec {
+        fourmolu-bin = pkgs.haskellPackages.fourmolu;
+        rustfmt-bin = pkgs.rustPlatform.rustfmt;
+        clickhouse-client-bin = pkgs.clickhouse-client;
+        helm-bin = pkgs.kubernetes-helm;
+        hyperdx-cli-bin = pkgs.hyperdx-cli;
+        argocd-bin = pkgs.argocd;
+
+        default = pkgs.mkShell {
+          name = "tools";
           packages = [
-            stack
-            ghcid
-            (haskell-language-server.override {
-              supportedGhcVersions = [ "96" ];
-            })
-            haskellPackages.fourmolu
+            fourmolu-bin
+            rustfmt-bin
+            clickhouse-client-bin
+            helm-bin
+            hyperdx-cli-bin
+            argocd-bin
           ];
         };
-      }
-    );
+
+        pkgs = import nixpkgs { system = self.system; };
+      };
+    };
 }
